c50520c9ac2ddf16fca6bd3a99dbd1fd24938d06
Alan Peter <alanpalamattam2@gmail.com>
2025-02-17 23:36:32 +0530

    yy

--- my-react-app/vite.config.ts
+++ my-react-app/vite.config.ts
@@ -1,25 +1,27 @@
 
-    import { defineConfig } from 'vite'
-    import react from '@vitejs/plugin-react'
-    import path from 'path'
-    
-    export default defineConfig({
-        plugins: [react()],
-        server: {
-            host: '0.0.0.0',
-            port: 5174,
-            strictPort: true,
-            hmr: {
-                clientPort: 5174
-            }
-        },
-        build: {
-            outDir: 'dist',
-            emptyOutDir: true
-        },
-        resolve: {
-            alias: {
-                '@': path.resolve(__dirname, './src')
-            }
-        }
-    })
+                        import { defineConfig } from 'vite'
+                        import react from '@vitejs/plugin-react'
+                        import path from 'path'
+                        
+                        export default defineConfig({
+                            plugins: [react()],
+                            root: 'C:/Users/alanp/CodeSyncProjects/default/my-react-app',
+                            publicDir: path.resolve('C:/Users/alanp/CodeSyncProjects/default/my-react-app', 'public'),
+                            server: {
+                                host: '0.0.0.0',
+                                port: 5174,
+                                strictPort: true,
+                                hmr: {
+                                    clientPort: 5174
+                                }
+                            },
+                            resolve: {
+                                alias: {
+                                    '@': path.resolve('C:/Users/alanp/CodeSyncProjects/default/my-react-app', 'src')
+                                }
+                            },
+                            optimizeDeps: {
+                                force: true
+                            },
+                            cacheDir: path.resolve('C:/Users/alanp/CodeSyncProjects/default/my-react-app', 'node_modules/.vite')
+                        })
